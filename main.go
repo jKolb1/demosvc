17ac8c9ef4dfa293f01fad8a0ecfa535f877224a
Jack Kolb <jkolb@berkeley.edu>
2016-05-24 13:31:35 -0700

    Service now publishes to bosswave uri

--- main.go
+++ main.go
@@ -2,12 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
 	"time"
+
+	bw2 "gopkg.in/immesys/bw2bind.v5"
+	yaml "gopkg.in/yaml.v2"
 )
 
 func main() {
-	for i := 0; i < 100; i++ {
-		fmt.Printf("hello world... %d\n", i)
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <num_repetitions>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	repetitions, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid repetitions argument:", err)
+		os.Exit(1)
+	}
+
+	bwClient := bw2.ConnectOrExit("127.0.0.1:28589")
+	bwClient.SetEntityFileOrExit("entity.key")
+
+	paramContents, err := ioutil.ReadFile("params.yml")
+	if err != nil {
+		fmt.Println("Failed to read parameters file:", err)
+		os.Exit(1)
+	}
+	parameters := make(map[string]string)
+	err = yaml.Unmarshal(paramContents, &parameters)
+
+	for i := 0; i < repetitions; i++ {
+		msg := fmt.Sprintf("%v: %s", i, parameters["msg"])
+		po := bw2.CreateTextPayloadObject(bw2.PONumText, msg)
+		bwClient.PublishOrExit(&bw2.PublishParams{
+			URI:            parameters["to"],
+			AutoChain:      true,
+			PayloadObjects: []bw2.PayloadObject{po},
+		})
+
 		time.Sleep(1 * time.Second)
 	}
 }
